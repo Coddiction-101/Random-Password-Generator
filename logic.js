bbf70da3441eec16d0b3a3b61045f78cc37c22b8
Gulshan Kushwaha <Gulshansays333@gmail.com>
2025-06-29 12:20:51 +0530

    Update logic.js
    JavaScript Code

---
+++ logic.js
@@ -0,0 +1,21 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //26
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"; //~
+const number = "0123456789";
+const symbol = "@#$%^()_+~|}{[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)]; // for the upperCase
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)]; //for the lowerCase
+  password += number[Math.floor(Math.random() * number.length)]; // for the number
+  password += symbol[Math.floor(Math.random() * symbol.length)]; // for the symbol
+
+  for (i = password.length; i < length; i++) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
